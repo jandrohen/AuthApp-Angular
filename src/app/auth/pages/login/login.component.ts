98e89314f62928551d20e0a25ce85a5f9964d66f
Alejandro Arguello <alejandro.arguello@bosonit.com>
2021-11-02 16:38:06 +0100

    Screen design of register and login of the app (course Angular lesson 387)

--- src/app/auth/pages/login/login.component.ts
+++ src/app/auth/pages/login/login.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import {FormBuilder, FormGroup, Validators} from '@angular/forms';
 
 @Component({
   selector: 'app-login',
@@ -6,11 +7,17 @@ import { Component, OnInit } from '@angular/core';
   styles: [
   ]
 })
-export class LoginComponent implements OnInit {
+export class LoginComponent{
 
-  constructor() { }
+  miFormulario: FormGroup = this.fb.group({
+    email   : ['test1@test1.com', [Validators.required, Validators.email]],
+    password: ['123456', [Validators.required, Validators.minLength(6)]],
+  });
+  constructor(private fb: FormBuilder) { }
 
-  ngOnInit(): void {
+  login(){
+    console.log(this.miFormulario.value);
+    console.log(this.miFormulario.valid);
   }
 
 }
