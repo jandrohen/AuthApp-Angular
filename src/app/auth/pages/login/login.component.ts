153f4f22a6a6dd07c08e1d687a95229ddba9b807
Alejandro Arguello <alejandro.arguello@bosonit.com>
2021-11-02 17:06:49 +0100

    Preparing the route navigation for dashboard protected of the app (course Angular lesson 388)

--- src/app/auth/pages/login/login.component.ts
+++ src/app/auth/pages/login/login.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import {FormBuilder, FormGroup, Validators} from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -13,11 +14,12 @@ export class LoginComponent{
     email   : ['test1@test1.com', [Validators.required, Validators.email]],
     password: ['123456', [Validators.required, Validators.minLength(6)]],
   });
-  constructor(private fb: FormBuilder) { }
+  constructor(private fb: FormBuilder,
+              private router: Router) { }
 
   login(){
     console.log(this.miFormulario.value);
-    console.log(this.miFormulario.valid);
+    this.router.navigateByUrl('/dashboard')
   }
 
 }
--- src/app/auth/pages/register/register.component.ts
+++ src/app/auth/pages/register/register.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import {FormBuilder, FormGroup, Validators} from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-register',
@@ -14,11 +15,12 @@ export class RegisterComponent  {
     email:    ['test4@test1.com',[Validators.required, Validators.email]],
     password: ['123456',[Validators.required, Validators.minLength(6)]],
   })
-  constructor(private fb: FormBuilder) { }
+  constructor(private fb: FormBuilder,
+              private router: Router) { }
 
   registro(){
     console.log(this.miFormulario.value);
-    console.log(this.miFormulario.valid);
+    this.router.navigateByUrl('/dashboard');
   }
 
 }
